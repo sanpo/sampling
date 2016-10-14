4ec71a30f34cc92f1a3fc3dcc741594b6c039b9b
sanpo <tka.sanpo@gmail.com>
2016-10-14 11:05:09 +0900

    Initial Commit

---
+++ jsonParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsonParse
+//
+//  Created by takakuwa shou on 2016/10/14.
+//  Copyright © 2016年 jsonParse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
